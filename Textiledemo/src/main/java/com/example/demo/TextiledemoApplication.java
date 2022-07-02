7dcad7306cc318899d44b7b8996a2d24c2cbf664
Praveen <Praveen@LAPTOP-SIL9FK58>
2022-07-02 17:20:33 +0530

    First Commit

---
+++ Textiledemo/src/main/java/com/example/demo/TextiledemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextiledemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextiledemoApplication.class, args);
+	}
+
+}
